6e57b641a45269574c4f30fa1c4c3b23d6803ed5
Vyrantino <17041279@itdurango.edu.mx>
2024-01-25 18:52:09 -0600

    adicion de dotenv y build

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config() ;
   const app = await NestFactory.create(AppModule);
   app.enableCors(); 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
